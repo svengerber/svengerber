a6c5854ff092fb110f7414d8c3696d4a19aafb8d
Sven Gerber <github@gerber.uno>
2024-12-10 22:12:05 +0100

    feat: update dev setup
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- dev_setup/wsl_base.sh
+++ dev_setup/wsl_base.sh
@@ -35,6 +35,7 @@ sudo apt install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ./dev_setup/home/.zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
