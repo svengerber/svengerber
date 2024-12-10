04b9df7a011d758c7ef73265dcf80ab5195809ca
Sven Gerber <github@gerber.uno>
2024-12-10 22:08:36 +0100

    feat: update dev setup
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- dev_setup/README.md
+++ dev_setup/README.md
@@ -16,12 +16,4 @@ chmod 600 ~/.ssh/id_*
 chmod 644 ~/.ssh/*.pub
 ```
 
-Import GPG Keys and add them to git config
-```
-gpg --batch --import gpg_*.key
-rm gpg_*.key
-
-gpg --list-secret-keys --keyid-format=long
--->  XXXX/<GPG Key ID>
---> add key to .gitconfig.git...
 ```
--- dev_setup/wsl_base.sh
+++ dev_setup/wsl_base.sh
@@ -12,8 +12,8 @@ cp ./dev_setup/home/.gitconfig* ~/
 #Kubectl current release
 sudo apt update
 sudo apt install -y ca-certificates curl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubectl
 
