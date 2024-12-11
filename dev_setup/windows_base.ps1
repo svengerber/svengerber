fd0a7f5c094a2c7e900eddfb3cd2aa17fb387670
Sven Gerber <github@gerber.uno>
2024-12-11 13:14:43 +0100

    feat: update dev setup
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- dev_setup/windows_base.ps1
+++ dev_setup/windows_base.ps1
@@ -1,33 +1,4 @@
-# List of application IDs to install
-$applications = @(
-    "OpenVPNTechnologies.OpenVPN"
-)
-
-# Function to install an application
-function Install-Application {
-    param (
-        [Parameter(Mandatory)]
-        [string]$AppId
-    )
-
-    Write-Host "Installing $AppId..." -ForegroundColor Cyan
-
-    # Run winget to install the application
-    try {
-        winget install --id $AppId --silent --accept-source-agreements --accept-package-agreements -e
-        if ($LASTEXITCODE -eq 0) {
-            Write-Host "$AppId installed successfully!" -ForegroundColor Green
-        } else {
-            Write-Host "Failed to install $AppId. Exit code: $LASTEXITCODE" -ForegroundColor Red
-        }
-    } catch {
-        Write-Host "An error occurred while installing $AppId: $_" -ForegroundColor Red
-    }
-}
-
-# Iterate over the application IDs and install each one
-foreach ($appId in $applications) {
-    Install-Application -AppId $appId
-}
-
-Write-Host "All installations complete!" -ForegroundColor Yellow
+winget install -e --id OpenVPNTechnologies.OpenVPN
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Citrix.Workspace
