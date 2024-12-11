05147a098e390237a38b1345615e7e9e4ccd99e5
Sven Gerber <github@gerber.uno>
2024-12-11 13:21:10 +0100

    feat: update dev setup
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- dev_setup/windows_base.ps1
+++ dev_setup/windows_base.ps1
@@ -2,3 +2,7 @@ winget install -e --id OpenVPNTechnologies.OpenVPN
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id SlackTechnologies.Slack
 winget install -e --id Citrix.Workspace
+winget install -e --id Jabra.Direct
+winget install -e --id MuhammedKalkan.OpenLens
+winget install -e --id Docker.DockerDesktop
+winget install -e --id WiresharkFoundation.Wireshark
