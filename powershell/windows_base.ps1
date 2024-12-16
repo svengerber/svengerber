3a469ebda3b789a3dd3bce99b2b234453a08b02c
Sven Gerber <github@gerber.uno>
2024-12-16 16:00:12 +0100

    feat: move Powershell and add new Apps
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- powershell/windows_base.ps1
+++ powershell/windows_base.ps1
@@ -1,3 +1,7 @@
+# SSH Agent
+Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
+
+# Tools
 winget install -e --id OpenVPNTechnologies.OpenVPN
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id SlackTechnologies.Slack
@@ -11,3 +15,4 @@ winget install -e --id JetBrains.IntelliJIDEA.Ultimate
 winget install -e --id VivaldiTechnologies.Vivaldi
 winget install -e --id Greenshot.Greenshot
 winget install -e --id Microsoft.PowerToys
+winget install -e --id Git.Git
