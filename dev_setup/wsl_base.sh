e9dcf542202dba3e54cae2f42482b3e745ba6ec3
Sven Gerber <github@gerber.uno>
2026-03-25 17:31:04 +0100

    feat: migrate from p10k to starship
    Signed-off-by: Sven Gerber <github@gerber.uno>

--- dev_setup/wsl_base.sh
+++ dev_setup/wsl_base.sh
@@ -35,7 +35,10 @@ sudo apt install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ./dev_setup/home/.zshrc ~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
